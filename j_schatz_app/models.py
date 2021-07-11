662dda23423f6cbefec9d502e40f22a836f5622f
marknovicio <marknovicio@gmail.com>
2021-07-10 18:56:05 -0700

    update contact database

--- j_schatz_app/models.py
+++ j_schatz_app/models.py
@@ -5,7 +5,9 @@ from django.db import models
 class UserMessage(models.Model):
     username = models.CharField(max_length=50)
     email = models.EmailField(max_length=100)
-    message = models.TextField(max_length=3000)
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
 
     def __repr__(self):
         return f'Name: {self.name} | email: {self.email} | message: {self.message}'
