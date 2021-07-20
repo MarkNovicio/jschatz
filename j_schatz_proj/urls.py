96686f2e16d070fcf6a2b531dfc177e922256896
marknovicio <marknovicio@gmail.com>
2021-07-19 18:53:42 -0700

    created login app database

--- j_schatz_proj/urls.py
+++ j_schatz_proj/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('j_schatz_app.urls')),
+    path('', include('login_app.urls'))
 ]
--- login_app/models.py
+++ login_app/models.py
@@ -1,14 +1,12 @@
 from django.db import models
 
-class User(model.Model):
+class User(models.Model):
     first_name = models.CharField(max_length=40)
     last_name = models.CharField(max_length=40)
     email = models.EmailField(max_length=100, unique = True)
     password = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add = True)
     updated_at = models.DateTimeField(auto_now = True)
-
-    objects = UserManager()
 
     def __repr__(self):
         return f'Name: {self.first_name} | email: {self.last_name} | message: {self.email}'
