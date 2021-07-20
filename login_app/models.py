0cd87d09f76e353d4e4dbffc0e61a8a914f9a32e
marknovicio <marknovicio@gmail.com>
2021-07-19 20:41:48 -0700

    update user login

--- login_app/models.py
+++ login_app/models.py
@@ -1,21 +1,30 @@
 from django.db import models
+import re
 
 class UserManager(models.Manager):
     def validator(self, post_data):
         
         errors = {}
+
+        email_regex = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+
         if len(post_data['first_name']) < 2:
             errors['first_name'] = "First name should be at least 2 characters long"
+        
         if len(post_data['last_name']) < 2:
             errors['last_name'] = "Last name should be at least 2 characters long"
+        
         if len(post_data['password']) < 8:
             errors['password'] = "Password should be at least 8 charcters long"
+        
         if post_data['password'] != post_data['confirm_password']:
-            errors['password'] = "Password does not match"
+            errors['password_match'] = "Password does not match"
 
+        if not email_regex.match(post_data['email']):
+            errors['email'] = "Invalid email address!")
 
         return errors
-
+    
 
 class User(models.Model):
     first_name = models.CharField(max_length=40)
--- login_app/views.py
+++ login_app/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render, redirect
+from .models import User
+from django.contrib import messages
 
-def register(request):
 
-    return render(request, 'sign_up.html')
+def register_user(request):
+    errors = User.objects.validator()
+    if errors:
+        for key, value in errors.items():
+            messages.error(request, value)
+        return redirect('user/registration')
+    return redirect('/login')
 # Create your views here.
