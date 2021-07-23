b7ca77157f11598e38bc1900cd3552c2c3b9d049
marknovicio <marknovicio@gmail.com>
2021-07-22 19:46:26 -0700

    updated User Model

--- login_app/models.py
+++ login_app/models.py
@@ -2,27 +2,26 @@ from django.db import models
 import re
 
 class UserManager(models.Manager):
-    def validator(self, post_data):
-        
-        errors = {}
+    def basic_validator(self, post_data):
+        errors={}
+       # today = datetime.today()
+        email_check = self.filter(email=post_data['email'])
+        if email_check:
+            errors['email'] = "Email already in use" 
+            #compares user email input to what is in database
 
         email_regex = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
-            
-        if len(post_data['first_name']) < 2:
-            errors['first_name'] = "First name should be at least 2 characters long"
-        
-        if len(post_data['last_name']) < 2:
-            errors['last_name'] = "Last name should be at least 2 characters long"
-            
         if not email_regex.match(post_data['email']):
             errors['email'] = "Invalid email address!"
-
+        if len(post_data['first_name']) < 2:
+            errors['first_name'] = "First name must have at least 2 characters"
+        if len(post_data['last_name']) < 2:
+            errors['last_name'] = "Last name must have at least 2 characters"
         if len(post_data['password']) < 8:
-            errors['password'] = "Password should be at least 8 charcters long"
+            errors['password'] = "Password must contain 8 characters"
+        if len(post_data['password']) < 0:
+            errors['password'] = "Password must contain 8 characters"
         
-        if post_data['password'] != post_data['confirm_password']:
-            errors['password_match'] = "Password does not match"
-
         return errors
     
 
