b59eee61fd1a51b0ba4479fdf3c9f63367fee337
marknovicio <marknovicio@gmail.com>
2021-07-29 14:41:32 -0700

    login error messages

--- login_app/urls.py
+++ login_app/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('registration', views.registration_form),
     path('user/registration', views.register_user),
     path('login_page', views.login_page),
+    path('user/login', views.login),
     path('forgot_password_page', views.forgot_password_page)
 ]
--- login_app/views.py
+++ login_app/views.py
@@ -30,6 +30,23 @@ def register_user(request):
 
 def login_page(request):
     return render(request, "login.html")
+
+def login(request):
+    signin_messages = messages
+    if request.method == "GET":
+        return redirect('/')
+    if not User.objects.basic_validator(request.POST['email'], request.POST['password']):
+        signin_messages.error(request, 'Invalid email/password')
+        return redirect('/user/signup')
+    
+    else:
+        logged_users = User.objects.filter(email=request.POST['email'])
+        user = logged_users[0]
+        request.session['user_id'] = user.id
+        request.session['first_name']= user.first_name
+        request.session['last_name']=user.last_name
+        request.session['email']= user.email
+        return redirect('/user/signup')
     
 def forgot_password_page(request):
     return render(request, "forgot_password.html")
