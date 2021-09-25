1199d88c367c87fb33f90b78b4953eb6bb33e960
marknovicio <marknovicio@gmail.com>
2021-09-24 18:52:52 -0700

    update login page

--- j_schatz_app/views.py
+++ j_schatz_app/views.py
@@ -10,6 +10,9 @@ def index(request):
 def about(request):
     return render(request, "about.html")
 
+def login(request):
+    return render(request, "login.html")
+
 def contact(request):
     
     return render(request, "contact.html")
